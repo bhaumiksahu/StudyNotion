092e5e036e9a933e8778509f53e31686dac7c4e2
Bhaumik Sahu <bhaumiksahu1207@gmail.com>
2024-05-15 19:20:11 +0530

    video , code section part done in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import HighlightText from "../components/core/HomePage/HighlightText"
 import CTAButton from "../components/core/HomePage/Button"
 import Banner from "../assets/Images/banner.mp4"
+import CodeBlocks from '../components/core/HomePage/CodeBlocks';
 const Home = () => {
   return (
     <div>
@@ -40,21 +41,105 @@ const Home = () => {
             </CTAButton>
         </div>
 
+        {/* video part */}
         <div className='shadow-blue-200 mx-3 my-14 shadow-[10px_-5px_30px_-5px]'>
            <video className="shadow-[20px_10px_rgba(255,255,255)]" muted loop autoPlay>
             <source src={Banner}/>
            </video>
         </div>
+
+        {/* codeSection 1 */}
+        <div>
+            <CodeBlocks position={"lg:flex"} heading={
+                <div className='text-4xl font-semibold'>
+                  Unlock Your
+                  <HighlightText text={" coding potential "}/>
+                  with our online course
+                </div>
+              } subheading={
+                "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+              }
+              ctabtn1={
+                {
+                  text:"try it yourself",
+                  linkto:"/signup",
+                  active:true
+                }
+              }
+              ctabtn2={
+                {
+                  text:"learn more",
+                  linkto:"/login",
+                  active:false
+                }
+              }
+              const codeBlockStyle ={ {
+                borderRadius: '100%',
+                filter: 'blur(34px)',
+                height: '250px',
+                left: '0.1%',
+                opacity: 0.15,
+                top: '0.1%',
+                width: '370px',
+                position:"absolute",
+                background: 'linear-gradient(123.77deg, #8a2be2 -6.46%, orange 59.04%, #f8f8ff 124.53%)'
+            }}
+           
+              codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
+              codeColor={"text-yellow-25"}
+            />
+        </div>
+        {/* codeSection 2 */}
+        <div >
+          <CodeBlocks position={"lg:flex-row-reverse"}
+          heading={
+            <div className='text-4xl font-semibold '>
+              Start 
+              <HighlightText text=" coding in seconds"/>
+            </div>
+          }  
+          subheading={
+            "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
+          }
+          ctabtn1={
+            {
+              text:"Continue Lesson",
+              active:true,
+              linkto:"/login"
+            }
+          }
+          ctabtn2={
+            {
+              text:"Learn More",
+              active:false,
+              linkto:"/signup"
+            }
+          } 
+          codeBlockStyle ={ {
+            borderRadius: '100%',
+            filter: 'blur(34px)',
+            height: '250px',
+            left: '0.1% ',
+            opacity: 0.15,
+            top: '0.1% ',
+            width: '370px',
+            position:"absolute",
+            background: 'linear-gradient(118.19deg, #1fa2ff -3.62%, #12d8fa 50.44%, #a6ffcb 104.51%)'
+        }}
+          codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+          />
+        </div>
       </div>
 
+        
 
 
-      {/* Section 1 */}
+      {/* Section 2 */}
 
 
 
 
-      {/* Section 1 */}
+      {/* Section 3 */}
 
 
 
